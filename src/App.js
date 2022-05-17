49585ea0cc6ecaa84f0c7811709becea143ef476
Paola Huyo <di.paolahuyo@gmail.com>
2022-05-17 11:16:24 -0500

    Tratando de hacer directamente la instalacion de react con algunos problemas de compilado, decidimos hacer un repositorio desde una carpeta directa

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "App.css";
+
+//Import Components
+import Nav from "./Nav";
+
+const App = () => (
+  <div>
+    <Nav/>
+    <h1 className="wow">Hello React!!!!!</h1>
+  </div>
+);
+
+export default App;
