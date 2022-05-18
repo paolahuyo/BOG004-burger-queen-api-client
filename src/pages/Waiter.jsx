e82ffe0c5eeab70af54feb91c07eabfc63c3b520
Paola Huyo <di.paolahuyo@gmail.com>
2022-05-17 21:10:42 -0500

    Cambio los estilos en waiter

--- src/pages/Waiter.jsx
+++ src/pages/Waiter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../components/stylesheets/Waiter.module.css';
+import logo from '../assets/logo-burger-queen.png';
 
 export default function Waiter() {
   return (
     <div className={styles.Waiter}>
+      <img src={logo} className={styles.Logo} alt="logo" />
       <h1 className={styles.h1}>Menu & Orders</h1>
       <p>
         <Link className={styles.Link} to="/">Home</Link>
