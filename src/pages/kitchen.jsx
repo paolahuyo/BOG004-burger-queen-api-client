b86d00334932f9d6a750bfc514add703c5ebe332
Paola Huyo <di.paolahuyo@gmail.com>
2022-05-26 13:03:10 -0500

    Agregando estilos en waiter.modules.css

--- src/pages/kitchen.jsx
+++ src/pages/kitchen.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { getOrder } from '../api/Products';
 import { Link } from 'react-router-dom';
 import styles from '../components/stylesheets/Waiter.module.css';
+import { CartProvider } from '../Context/CartContext';
 
 export default function Kitchen() {
 
@@ -9,44 +10,47 @@ export default function Kitchen() {
 
   useEffect(() => {
     getOrder()
-    .then((res) => {
-      console.log(res.data)
-      setOrders(res.data)
-    })
-    .catch()
+      .then((res) => {
+        console.log(res.data)
+        setOrders(res.data)
+      })
+      .catch()
   }, [])
-  if(orders !== undefined && orders[0] !== undefined)  console.log("test " + JSON.stringify(orders[0].products[0].product.price));
+  if (orders !== undefined && orders[0] !== undefined) console.log("test " + JSON.stringify(orders[0].products[0].product.price));
   return (
-    <div className={styles.Bigbox}>
-      <h1>Página de cocina</h1>
-      <p>
-        <Link to="/">Sign Out</Link>
-      </p>
-      {orders.map((order, i)=>(
-        <div className='card' key={order.id}>
-          <h2 className={styles.Orderstext}>ORDER</h2>
-        <div className='card-body'>
-            <p className={styles.Orderstext}>{order.client}</p>
-            <p className={styles.Orderstext}>{order.status}</p>
-            {order.dateProcessed &&  <p className={styles.Delivered}> Delivered: {order.dateProcessed}</p>}
-            <p className={styles.Orderstext}> Created: {order.dataEntry}</p>
-            <ul className="list-group list-group-flush">
-              <li className={styles.Orderstext}>{order.products.qty}</li>
-              <li className={styles.Orderstext}>{orders.products}</li>
-              <li className={styles.Orderstext}>{orders[i].products[i].product.name} {orders[i].products[i].product.price}</li>
-              <li className={styles.Orderstext}>{orders[1].products[1].product.name} {orders[1].products[1].product.price}</li>
-            </ul>
-            {!order.dateProcessed &&  <div className='card-body'>
-              <a href="#" className='card-link'>READY TO DELIVER</a>
-             </div>
-            }
-
-        </div>
-
-        </div>
-      ))}
-    </div>
-  );
+    <CartProvider>
+      <section className={styles.Box}>
+        <h1>Página de cocina</h1>
+        {orders.map((order, i) => (
+          <section className='all-cards' key={order.id} >
+          <div className='card' style={{width:400}}>
+            <h2 className={styles.Orderstext}>ORDER</h2>
+            <div className='card-body'>
+              <p className={styles.Orderstext}>{order.client}</p>
+              <p className={styles.Orderstext}>{order.status}</p>
+              {order.dateProcessed && <p className={styles.Delivered}> Delivered: {order.dateProcessed}</p>}
+              <p className={styles.Orderstext}> Created: {order.dataEntry}</p>
+              <ul className="list-group list-group-flush">
+                <li className={styles.Orderstext}>{order.products.qty}</li>
+                <li className={styles.Orderstext}>{orders.products}</li>
+                <li className={styles.Orderstext}>{orders[i].products[i].product.name} {orders[i].products[i].product.price}</li>
+                <li className={styles.Orderstext}>{orders[1].products[1].product.name} {orders[1].products[1].product.price}</li>
+              </ul>
+              {!order.dateProcessed &&
+              <div className='card-body'>
+                <a href="#" className='card-link'>READY TO DELIVER</a>
+              </div>
+              }
+            </div>
+          </div>
+          </section>
+        ))}
+        <p>
+          <Link to="/">Sign Out</Link>
+        </p>
+      </section>
+    </CartProvider>
+    );
 }
 
 
