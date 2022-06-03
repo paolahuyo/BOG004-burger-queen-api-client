198f24cb5b8b7a6a156be540cd4af825ce470907
Dan13l4 <kdbaron426@gmail.com>
2022-06-03 11:01:28 -0500

    creando tests de login

--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import CartContext from '../Context/CartContext';
 import plusIcon from '../assets/plus-white.png';
+import minusIcons from "../assets/minus-sign.png";
 
 const ItemCart = ({item}) => {
     const {deleteItemToCart, addItemToCart} = useContext(CartContext);
@@ -10,8 +11,8 @@ const ItemCart = ({item}) => {
                 <th scope="row">{item.name}</th>
                 <th scope="row">{item.amount}</th>
                 <th scope="row">${item.price*item.amount}</th>
-                <th scope="row"><button className="btn btn-sm btn-success" onClick={()=> {addItemToCart(item)}}><span src={plusIcon}></span>Add</button>
-                <button class="btn btn-sm btn-danger"  onClick={()=> {deleteItemToCart(item)}}>Delete</button></th>
+                <th scope="row"><button className="btn btn-sm btn-success" onClick={()=> {addItemToCart(item)}}>Add <img src={plusIcon} alt="btn plus" style={{width:15, alignSelf:'center'}} /> </button>
+                <button class="btn btn-sm btn-danger"  onClick={()=> {deleteItemToCart(item)}}>Delete <img src={minusIcons} alt="btn plus" style={{width:15, alignSelf:'center'}} /></button></th>
             </tr>
     );
 }
