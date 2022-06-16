0192876de9568f9185adbe60e5a9c8ec16f6f277
Dan13l4 <kdbaron426@gmail.com>
2022-06-16 09:02:42 -0500

    creando modal para editar el usuario, todavia no funciona la funcion de edit user

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBarAdmin from '../components/NavBarAdmin';
 import CreateUsers from './CreateUsers';
-import SeeAllUsers from '../components/SeeAllUsers';
 
 export default function Admin() {
   return (
